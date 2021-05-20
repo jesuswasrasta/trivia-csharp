5638f6225f629ee42b624a55d4fe330f243a4df2
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2021-05-20 23:13:32 +0200

    Trivia kata
    Original version ported to .NET Core 5.0

---
+++ Trivia.Core.Tests/SetupTest.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Trivia.Core;
+using NUnit.Framework;
+
+namespace Trivia.Core.Test
+{
+    public class SetupTest
+    {
+        [Test]
+        public void SetupIsGood()
+        {
+            Assert.True(true);
+        }
+   }
+}
